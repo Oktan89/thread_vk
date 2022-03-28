8e25a7ca260d32060f4954670766dfef88aef639
Zhigalenko <r.zhigalenko@mail.ru>
2022-03-28 16:51:15 +0500

    completed

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
 
-
+std::mutex mut;
+std::condition_variable cv;
+bool sw = false;
 
 int main()
 {
+    auto thread1 = [](){
+       while(true)
+       {
+            std::unique_lock<std::mutex> ls(mut);
+            cv.wait(ls, [](){return sw;});
+            std::cout << "2]";
+            sw = false;
+            cv.notify_one();
+       }
+    };
 
+    auto thread2 = [](){
+        while(true)
+        {
+            std::unique_lock<std::mutex> ls2(mut);
+            cv.wait(ls2, [](){return !sw;});
+                std::cout << "[1";
+                sw = true;
+            cv.notify_one();
+        }
+    };
+
+    std::thread(thread1).join();
+    std::thread(thread2).join();
 
     return 0; 
 }
